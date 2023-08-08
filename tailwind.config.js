bf1b96f00885b0a1e9c66d00b30fe87c33c9ce67
Patrick Luz <eu@patrickluz.dev>
2023-08-08 03:58:47 -0300

    chore: config figma colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "fig-black": "#000112",
+        "fig-dark-grey": "#2B2C37",
+        "fig-light-grey": "#F4F7FD",
+        "fig-lines-dark": "#3E3F4E",
+        "fig-lines-light": "#E4EBFA",
+        "fig-main-purple": "#635FC7",
+        "fig-main-purple-hover": "#A8A4FF",
+        "fig-medium-grey": "#828FA3",
+        "fig-red": "#EA5555",
+        "fig-red-hover": "#FF9898",
+        "fig-very-dark-hover": "#20212C"
+      }
+    },
   },
   plugins: [],
 }
